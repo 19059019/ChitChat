73d9fd44a972f3f507d1ccd4a2d48020e8964125
Michael Shepherd <michael.shepherd.contact@gmail.com>
2018-08-01 15:29:02 +0200

    Multiple clients can join and chat

--- java_files/clientInstance.java
+++ java_files/clientInstance.java
@@ -5,53 +5,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
 
-public class Server {
-  private static ServerSocket server = null;
-  private static Socket client = null;
-  private static final int clientLimit = 5;
-  private static final clientInstance[] clientThreads = new clientInstance[clientLimit];
-  private static PrintStream output = null;
-  private static Boolean status = true;
-
-  public static void main(String[] args) {
-    int port = 8000;
-
-    // open ServerSocket
-    try {
-      server = new ServerSocket(port);
-    } catch(IOException e) {
-      System.err.println(e);
-    }
-
-    // create new socket for each new client that attempts to connect
-    while (status) {
-      int i;
-      try{
-        client = server.accept();
-        for (i = 0; i < clientLimit; i++) {
-          if (clientThreads[i] == null) {
-            clientThreads[i] = new clientInstance(client, clientThreads);
-            clientThreads[i].start();
-            break;
-          }
-        }
-
-        // Message if too many clients have connected
-        if (i == clientLimit) {
-          output = new PrintStream(client.getOutputStream());
-          output.println("Chit Chat Chatroom full, Unlucky!");
-          output.close();
-          client.close();
-        }
-      } catch (IOException e) {
-        System.err.println(e);
-      }
-
-    }
-  }
-
-}
-
 class clientInstance extends Thread {
 
   private DataInputStream is = null;
@@ -68,6 +21,7 @@ class clientInstance extends Thread {
 
   public void run() {
     clientInstance[] clientThreads = this.clientThreads;
+
     int clientLimit = this.clientLimit;
 
     try {
